fde321af38d7368d364df5e598e461ff7dfd9452
Franco Redivo <franco161101@gmail.com>
2026-06-11 14:53:57 -0300

    feat: add blog s service to define hardcoded blogs and a funtion to get all blogs

---
+++ app/services/blogs.ts
@@ -0,0 +1,33 @@
+const blogs = [
+    {
+        id: 1, 
+        title: "SQL vs NOSQL",
+        author: "Tony Soprano",
+        url: "randomurl.com",
+        likes: 6
+    },
+    {
+        id: 2,
+        title: "react testing",
+        author: "Bobby Bacala",
+        url: "anotherurl.com",
+        likes: 12
+    },
+    {
+        id: 3,
+        title: "tailwind tips",
+        author: "Johnny Sacks",
+        url: "Aurl.com",
+        likes: 10
+    }
+];
+
+let nextId = 4;
+
+export const getBlogs = () => {
+    return blogs;
+}
+
+export const addBlogs = (title: string, author: string, url: string, likes: number) => {
+    blogs.push({id: nextId++, title, author, url, likes});
+}
