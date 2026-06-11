c5da901bb3f9dd73de08144ece50539cd45f28b4
Franco Redivo <franco161101@gmail.com>
2026-06-11 18:33:13 -0300

    feat: add search input to filter blogs based on title

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,26 +1,38 @@
-import { getBlogs } from "../services/blogs";
+import { getBlogs, filterBlogsByTitle } from "../services/blogs";
+import { filterBlogs } from "../actions/blogs";
 import Link from "next/link";
 
-const Blogs = () => {
+const Blogs = async ({ searchParams, }: {searchParams: Promise<{ filter?: string }>}) => {
+    const { filter } = await searchParams;
     const blogs = getBlogs();
-    const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
+    const filteredBlogs = filter ? filterBlogsByTitle(filter) : blogs;
+    const sortedBlogs = [...filteredBlogs].sort((a, b) => b.likes - a.likes);
+
     return (
         <div>
             <h2>Blogs</h2>
-            <ul>
-                {sortedBlogs.map(blog => (
-                    <li key={blog.id}>
-                        <Link href={`/blogs/${blog.id}`}>
-                            {blog.title}
-                        </Link>
-                        <div>
-                            <p>Author: {blog.author}</p>
-                            <p>URL: {blog.url}</p>
-                            <p>Likes: {blog.likes}</p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <form action={filterBlogs}>
+                    <input type="text" name="filter" placeholder="Filter by title" defaultValue={filter} />
+                    <button type="submit">Apply</button>
+                </form>
+            </div>
+            <div>
+                <ul>
+                    {sortedBlogs.map(blog => (
+                        <li key={blog.id}>
+                            <Link href={`/blogs/${blog.id}`}>
+                                <h3>{blog.title}</h3>
+                            </Link>
+                            <div>
+                                <p>Author: {blog.author}</p>
+                                <p>URL: {blog.url}</p>
+                                <p>Likes: {blog.likes}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
