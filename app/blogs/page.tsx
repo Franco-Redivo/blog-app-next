a1157297a3358765c42fc8716b3fd1000e75aed5
Franco Redivo <franco161101@gmail.com>
2026-06-11 17:14:03 -0300

    feat: add link to individual blog view

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,4 +1,5 @@
 import { getBlogs } from "../services/blogs";
+import Link from "next/link";
 
 const Blogs = () => {
     const blogs = getBlogs();
@@ -8,8 +9,10 @@ const Blogs = () => {
             <ul>
                 {blogs.map(blog => (
                     <li key={blog.id}>
+                        <Link href={`/blogs/${blog.id}`}>
+                            {blog.title}
+                        </Link>
                         <div>
-                            <h3>{blog.title}</h3>
                             <p>Author: {blog.author}</p>
                             <p>URL: {blog.url}</p>
                             <p>Likes: {blog.likes}</p>
