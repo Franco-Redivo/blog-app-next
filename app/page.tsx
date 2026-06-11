3dedc643603af509af9c0d9cad5328daa671eb29
Franco Redivo <franco161101@gmail.com>
2026-06-11 14:52:00 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Blogs app</h1>
+      <p>A app for <a href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs">Full Stack Open Next.js</a></p>
+
+    </div>
+  );
+}
