c81abec5843de72fbc8c765878dedfc13ecbb132
Franco Redivo <franco161101@gmail.com>
2026-06-16 15:04:19 -0300

    feat: add error handling when registering a new user

--- app/actions/users.ts
+++ app/actions/users.ts
@@ -4,11 +4,20 @@ import { redirect } from "next/navigation";
 import bcrypt from "bcryptjs";
 import { db } from "../../db";
 import { users } from "../../db/schema";
+import { getUserByUsername } from "../services/users";
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (prevState: {error: string, values: {username: string, name: string, password: string}}, formData: FormData) => {
     const username = (formData.get("username") as string)?.trim();
     const name = (formData.get("name") as string)?.trim();
     const password = formData.get("password") as string
+    const passwordConfirm = formData.get("passwordConfirm") as string;
+
+    if (!password || password.length < 4) return {error: "password must be at least 4 characters long", values: {username, name, password}};
+    if (password !== passwordConfirm) return {error: "passwords fields do not match", values: {username, name, password}};
+    if (!username || username.length < 4) return {error: "username must be atleast 4 characters long", values: {username, name, password}};
+
+    const usernameExists = await getUserByUsername(username);
+    if (usernameExists) return {error: "username already exists", values: {username, name, password}}
 
     const passwordHash = await bcrypt.hash(password, 10);
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,30 +1,40 @@
+"use client"
 import Link from "next/link";
 import { registerUser } from "../actions/users";
+import { useActionState } from "react";
 
 export default function RegisterPage() {
+    const [state, formAction] = useActionState(registerUser, {error: "", values: {username: "", name: "", password: ""}});
     return (
         <div>
             <h2>Register</h2>
-            <form action={registerUser}>
+            <form action={formAction}>
                 <div>
                     <label>
                         Username
-                        <input type="text" name="username" required/>
+                        <input type="text" name="username" required minLength={4} defaultValue={state.values?.username}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Name
-                        <input type="text" name="name" required/>
+                        <input type="text" name="name" required defaultValue={state.values?.name}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Password
-                        <input type="password" name="password" required/>
+                        <input type="password" name="password" required minLength={4} defaultValue={state.values?.password}/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Confirm Password
+                        <input type="password" name="passwordConfirm" required minLength={4}/>
                     </label>
                 </div>
                 <button type="submit">Register</button>
+                {state.error && <p style={{color: "red"}}>{state.error}</p>}
             </form>
         </div>
     );
