3d2ae55b34bd4b8dd8912419e6616b4dfe28f32d
Franco Redivo <franco161101@gmail.com>
2026-06-16 15:03:46 -0300

    feat: add error handling when creating a new blog

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation"
 import { addBlogs, incrementBlogLikes} from "../services/blogs"
 import { revalidatePath } from "next/cache"
 
-export const createBlog = async (formData: FormData) => {
+export const createBlog = async (prevState: {error: string}, formData: FormData) => {
     const session = await auth();
     if( !session) {
         redirect("/login");
@@ -14,6 +14,10 @@ export const createBlog = async (formData: FormData) => {
     const author = formData.get("author") as string;
     const url = formData.get("url") as string;
     const likes = parseInt(formData.get("likes") as string, 10);
+
+    if (!title || title.length < 5) return { error: "title must be at least 5 characters long", values:{title, author, url, likes} };
+    if (!author || author.length < 5) return { error: "author must be at least 5 characters long", values:{title, author, url, likes}};
+    if (!url || url.length < 5) return { error: "url must be at least 5 characters long",values:{title, author, url, likes} };
 
     await addBlogs(title, author, url, likes);
     revalidatePath("/blogs");
--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,35 +1,40 @@
-import { createBlog } from "../../actions/blogs"
+"use client"
+
+import { createBlog } from "../../actions/blogs";
+import { useActionState } from "react";
 
 const NewBlog = () => {
+    const [state, formAction] = useActionState(createBlog, {error: "", values: {title: "", author: "", url: "", likes: 0}});
     return (
         <div>
             <h2>Create a new blog</h2>
-            <form action={createBlog}>
+            <form action={formAction}>
                 <div>
                     <label>
                         Title
-                        <input type="text" name="title" required />
+                        <input type="text" name="title" required minLength={5} defaultValue={state.values?.title}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Author
-                        <input type="text" name="author" required />
+                        <input type="text" name="author" required minLength={5} defaultValue={state.values?.author}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         URL
-                        <input type="text" name="url" required />
+                        <input type="text" name="url" required minLength={5} defaultValue={state.values?.url}/>
                     </label>
                 </div>
                 <div>
                     <label>
                         Likes
-                        <input type="number" name="likes" required />
+                        <input type="number" name="likes" required defaultValue={state.values?.likes}/>
                     </label>
                 </div>
                 <button type="submit">Create</button>
+                {state.error && <p style={{color: "red"}}>{state.error}</p>}
             </form>
         </div>
     )
