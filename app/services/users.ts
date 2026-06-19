888ab36b6dd79ca6b79ca25f82e5e67fb77e1e26
Franco Redivo <franco161101@gmail.com>
2026-06-19 20:02:12 -0300

    feat: add getUsersBlogs query to fetch user info and attach their blogs

--- app/services/users.ts
+++ app/services/users.ts
@@ -1,5 +1,5 @@
 import { db } from "../../db";
-import { users } from "../../db/schema";
+import { users, blogs } from "../../db/schema";
 import { eq } from "drizzle-orm"
 
 export const getUsers = async () => {
@@ -17,4 +17,23 @@ export const getUserByUsername = async (username: string) => {
     return db.query.users.findFirst({
         where: eq(users.username, username)
     })
+}
+
+export const getUserBlogs = async (username: string) => {
+    const rows = await db.select().from(users).leftJoin(blogs, eq(users.id, blogs.userId)).where(eq(users.username, username));
+    if (rows.length === 0) {
+        return null;
+    }
+    const user = rows[0].users;
+
+    return {
+        ...user,
+        blogs: rows.filter(r => r.blogs !== null).map(r => r.blogs),
+    };
+}
+
+export const getUserWithToken = async (token: string) => {
+    return db.query.users.findFirst({
+        where: eq(users.token, token)
+    });
 }
