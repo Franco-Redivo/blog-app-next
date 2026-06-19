344591041129074fbd4d298bb44ae68fc8fc921b
Franco Redivo <franco161101@gmail.com>
2026-06-19 20:00:22 -0300

    feat: add styling to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Blogs app</h1>
-      <p>A app for <a href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs">Full Stack Open Next.js</a></p>
+    <div className="max-w-3xl mx-auto p-6 flex flex-col gap-4 items-center my-6">
+      <h1 className="text-3xl font-bold">Blogs app</h1>
+      <p className="text-lg">A app for <a href="https://courses.mooc.fi/org/uh-cs/courses/full-stack-open-nextjs" className="text-blue-500 hover:underline">Full Stack Open Next.js</a></p>
 
     </div>
   );
