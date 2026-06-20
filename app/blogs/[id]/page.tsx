ee5d3f8ff6ce37f4c6d028f2d153516b9bbcb4ee
Franco Redivo <franco161101@gmail.com>
2026-06-20 16:54:32 -0300

    feat: add button to add blog to user's reading list

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,12 +1,18 @@
 import { notFound } from "next/navigation";
-import { getBlogById } from "../../services/blogs";
-import { likeBlog } from "../../actions/blogs";
+import { getBlogById, getReadingListEntry } from "../../services/blogs";
+import { likeBlog, addBlogToReadingList } from "../../actions/blogs";
+import { getCurrentUser } from "../../services/session";
 
 const BlogPage = async ({params}: {params: Promise<{id: string}>}) => {
     const {id} = await params;
     const blog = await getBlogById(Number(id));
+    const user = await getCurrentUser();
+    const readingListEntry = user ? await getReadingListEntry(Number(id)) : null;
 
     if(!blog) {
+        notFound();
+    }
+    if (!user) {
         notFound();
     }
 
@@ -18,12 +24,26 @@ const BlogPage = async ({params}: {params: Promise<{id: string}>}) => {
                 <p>URL: {blog.url}</p>
                 <p>Likes: {blog.likes}</p>
             </div>
-            <form action={likeBlog} className="self-end">
-                <input type="hidden" name="id" value={blog.id} />
-                <button type="submit" className="bg-blue-500 text-white text-lg hover:bg-blue-600 px-4 py-2 rounded">
-                    Like
-                </button>
-            </form>
+            <div>
+                <form action={likeBlog} className="self-end">
+                    <input type="hidden" name="id" value={blog.id} />
+                    <button type="submit" className="bg-blue-500 text-white text-lg hover:bg-blue-600 px-4 py-2 rounded">
+                        Like
+                    </button>
+                </form>
+                {user.id !== blog.userId && !readingListEntry && (
+                    <form action={addBlogToReadingList} className="self-end mt-2">
+                        <input type="hidden" name="id" value={blog.id} />
+                        <button type="submit" className="bg-green-500 text-white text-lg hover:bg-green-600 px-4 py-2 rounded">
+                            Add to Reading List
+                        </button>
+                    </form>
+                )}
+                {readingListEntry && (
+                    <p className="mt-2 text-sm text-gray-600">This blog is in your reading list</p>
+                )}
+
+            </div>
         </div>
     )
 }
