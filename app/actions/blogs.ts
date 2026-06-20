fcfe276733d84b8a88219527e24b8061d98f8652
Franco Redivo <franco161101@gmail.com>
2026-06-20 16:53:43 -0300

    feat: add action to toggle reading status

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -2,7 +2,7 @@
 import { auth } from "@/auth"
 
 import { redirect } from "next/navigation"
-import { addBlogs, incrementBlogLikes} from "../services/blogs"
+import { addBlogs, incrementBlogLikes, addToReadingList, toggleReadStatus, getBlogIdByInfo} from "../services/blogs"
 import { revalidatePath } from "next/cache"
 
 export const createBlog = async (prevState: {error: string, success: boolean, values:{title: string, author: string, url: string, likes: number}}, formData: FormData) => {
@@ -20,6 +20,10 @@ export const createBlog = async (prevState: {error: string, success: boolean, va
     if (!url || url.length < 5) return { error: "url must be at least 5 characters long",success: false, values:{title, author, url, likes} };
 
     await addBlogs(title, author, url, likes);
+    const blogId = await getBlogIdByInfo(title, author, url);
+
+    await addToReadingList(blogId!);
+    await toggleReadStatus(blogId!);
     revalidatePath("/blogs");
     //redirect("/blogs");
     return {error: "", success:true, values:{title, author, url, likes} };
@@ -37,4 +41,16 @@ export const filterBlogs = async (formData: FormData) => {
     const filter = formData.get("filter") as string;
     redirect(`/blogs?filter=${encodeURIComponent(filter)}`);
 
+}
+
+export const addBlogToReadingList = async (formData: FormData) => {
+    const id = Number(formData.get("id"));
+    await addToReadingList(id);
+    revalidatePath(`/blogs/${id}`);
+}
+
+export const toggleBlogReadStatus = async (formData: FormData) => {
+    const id = Number(formData.get("id"));
+    await toggleReadStatus(id);
+    revalidatePath("/me");
 }
